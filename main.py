7429a84f738475e0edd4f7390310d26a10cb85e7
omnisonic <omnisonic@gmail.com>
2022-10-01 10:16:03 -0600

    add readme and script

---
+++ main.py
@@ -0,0 +1,12 @@
+from playsound import playsound
+
+import time
+
+while True:
+    playsound('bell.mp3')
+    time.sleep(900)
+
+
+
+
+    print('playing sound using playsound')
---
+++ readme.md
@@ -0,0 +1,5 @@
+# A simple timer that plays a sound every x seconds. 
+
+A python timer that can be used to remind oneself to switch tasks or take the cake out of the oven.
+
+
